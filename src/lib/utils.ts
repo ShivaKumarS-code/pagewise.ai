0b6c4503465914ca7026c2b31ef47fa0b6bb8dd8
Shiva Kumar S <sk9778626@gmail.com>
2025-07-27 19:39:02 +0530

    fix

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -10,9 +10,7 @@ export function absoluteUrl(path: string) {
   if (typeof window !== 'undefined') return path
   if (process.env.VERCEL_PROJECT_PRODUCTION_URL)
     return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}${path}`
-  return `http://localhost:${
-    process.env.PORT ?? 3000
-  }${path}`
+  return `http://localhost:${process.env.PORT ?? 3000}${path}`
 }
 
 export function constructMetadata({
